c5d20aaf36d8afce51d80657cebc63732eccddb7
roboto84 <7658704+roboto84@users.noreply.github.com>
2021-03-16 18:23:03 -0400

    feat(lexicon): add endpoint for lexicon messages

--- dash_api/dash_api.py
+++ dash_api/dash_api.py
@@ -1,8 +1,9 @@
 import os
 import logging.config
 import uvicorn
+import ast
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from server import Server
 from wh00t_core.library.client_network import ClientNetwork
@@ -13,14 +14,46 @@ app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True,
                    allow_methods=['*'], allow_headers=['*'])
 
 
-@app.get('/')
+@app.get('/', status_code=status.HTTP_200_OK)
 def home():
-    return {'message': socket_network.get_message_history()}
+    return {
+        'message': socket_network.get_message_history()
+    }
 
 
-@app.get('/air/')
-def home():
-    return {'message': list(filter(lambda key: key['id'] == 'air_bot', socket_network.get_message_history()))}
+@app.get('/air/', status_code=status.HTTP_200_OK)
+def air_home():
+    return {
+        'message': list(filter(lambda key: key['id'] == 'air_bot', socket_network.get_message_history()))
+    }
+
+
+@app.get('/lexicon/', status_code=status.HTTP_200_OK)
+def lexicon_home():
+    return {
+        'message': list(filter(lambda key: key['id'] == 'lexicon_bot', socket_network.get_message_history()))
+    }
+
+
+@app.get('/lexicon/word_search/{search_word}', status_code=status.HTTP_200_OK)
+def lexicon_word_search(search_word: str):
+    socket_network.send_message('get_word_definition', str({'search_word': search_word}))
+    definition_ready = False
+    latest_message_unpacked = {}
+
+    while not definition_ready:
+        messages = list(filter(lambda key: key['id'] == 'lexicon_bot', socket_network.get_message_history()))
+        last_messages_index = len(messages)-1
+        if last_messages_index >= 0:
+            latest_message = messages[last_messages_index]
+            latest_message_unpacked: dict = ast.literal_eval(latest_message['message'])
+            if 'word' in latest_message_unpacked and latest_message_unpacked['word'] == search_word:
+                definition_ready = True
+
+    return {
+        'search_word': search_word,
+        'definition': latest_message_unpacked
+    }
 
 
 if __name__ == '__main__':
