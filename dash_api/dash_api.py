94a11afd99a4160a88af8035444ed714f2e3ac12
roboto84 <7658704+roboto84@users.noreply.github.com>
2021-08-25 06:54:49 -0400

    refactor(lexicon): update to use cached results

--- dash_api/dash_api.py
+++ dash_api/dash_api.py
@@ -2,14 +2,14 @@ import os
 import logging.config
 import uvicorn
 import ast
+import time
 from dotenv import load_dotenv
 from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from server import Server
 from wh00t_core.library.client_network import ClientNetwork
 
-origins = ['http://127.0.0.1:8080/']
-app = FastAPI()
+app: FastAPI = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True,
                    allow_methods=['*'], allow_headers=['*'])
 
@@ -38,38 +38,53 @@ def lexicon_home():
 @app.get('/lexicon/word_search/{search_word}', status_code=status.HTTP_200_OK)
 def lexicon_word_search(search_word: str):
     socket_network.send_message('get_word_definition', str({'search_word': search_word}))
-    definition_ready = False
-    latest_message_unpacked = {}
+    definition_ready: bool = False
+    message_unpacked: dict = {}
+    max_loops: int = 20
+    max_loop_counter: int = 0
 
-    while not definition_ready:
+    while not definition_ready and max_loop_counter < max_loops:
         messages = list(filter(lambda key: key['id'] == 'lexicon_bot', socket_network.get_message_history()))
-        last_messages_index = len(messages)-1
-        if last_messages_index >= 0:
-            latest_message = messages[last_messages_index]
-            latest_message_unpacked: dict = ast.literal_eval(latest_message['message'])
-            if 'word' in latest_message_unpacked and latest_message_unpacked['word'] == search_word:
+        for message in messages:
+            message_unpacked: dict = ast.literal_eval(message['message'])
+            if 'word' in message_unpacked and message_unpacked['word'] == search_word:
                 definition_ready = True
+                break
+        max_loop_counter += 1
+        time.sleep(.25)
 
-    return {
+    return_object: dict = {
         'search_word': search_word,
-        'definition': latest_message_unpacked
+        'definition': message_unpacked
     }
+    if max_loop_counter == max_loops:
+        return_object['definition'] = {
+            'word': search_word,
+            'part_of_speech': 'unknown',
+            'pronounce': 'unknown',
+            'audio': '',
+            'definition': ['word was not found.'],
+            'example': 'none'
+        }
+    return return_object
 
 
 if __name__ == '__main__':
     logging.config.fileConfig(fname=os.path.abspath('dash_api/bin/logging.conf'), disable_existing_loggers=False)
-    logger = logging.getLogger(__name__)
+    logger: logging.Logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
     try:
         load_dotenv()
-        HOST_SERVER_ADDRESS = os.getenv('HOST_SERVER_ADDRESS')
-        SOCKET_SERVER_PORT = int(os.getenv('SOCKET_SERVER_PORT'))
-        HTTP_SERVER_PORT = int(os.getenv('HTTP_SERVER_PORT'))
+        HOST_SERVER_ADDRESS: str = os.getenv('HOST_SERVER_ADDRESS')
+        SOCKET_SERVER_PORT: int = int(os.getenv('SOCKET_SERVER_PORT'))
+        HTTP_SERVER_PORT: int = int(os.getenv('HTTP_SERVER_PORT'))
 
-        socket_network = ClientNetwork(HOST_SERVER_ADDRESS, SOCKET_SERVER_PORT, 'dash_api', 'app', logging)
-        config = uvicorn.Config(app, host=HOST_SERVER_ADDRESS, port=HTTP_SERVER_PORT, log_level='info', loop='asyncio')
-        server = Server(config=config)
+        socket_network: ClientNetwork = ClientNetwork(HOST_SERVER_ADDRESS, SOCKET_SERVER_PORT,
+                                                      'dash_api', 'app', logging)
+        config: uvicorn.Config = uvicorn.Config(app, host=HOST_SERVER_ADDRESS, port=HTTP_SERVER_PORT,
+                                                log_level='info', loop='asyncio')
+        server: Server = Server(config=config)
 
         with server.run_in_thread():
             socket_network.sock_it()
