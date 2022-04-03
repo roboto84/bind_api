730abb6b5e449e015202d85f3d9e609c3e8da607
roboto84 <7658704+roboto84@users.noreply.github.com>
2022-04-03 14:44:46 -0400

    fix(wh00t_chat): silence alert on web sock connect
    The chat alert on user connection seemed like a good
    idea, but polluting the chat with such messages seems
    like an anti-pattern.  A solution like a user
    availability window would seem better.

--- roboto_api/routes/wh00t_chat_route.py
+++ roboto_api/routes/wh00t_chat_route.py
@@ -18,7 +18,7 @@ class ChatApi:
             lambda key: key['category'] == 'chat_message', self.web_sock_manager.get_wh00t_history()))
         message_history = [self.web_sock_manager.create_web_sock_package(message) for message in message_history]
         await self.web_sock_manager.send_web_sock_message(message_history, websocket)
-        self.web_sock_manager.send_wh00t_message(client_id, f'Hi, I just connected.')
+        # self.web_sock_manager.send_wh00t_message(client_id, f'Hi, I just connected.')
 
         try:
             while True:
@@ -26,4 +26,4 @@ class ChatApi:
                 self.web_sock_manager.send_wh00t_message(client_id, web_sock_message)
         except WebSocketDisconnect:
             self.web_sock_manager.disconnect(websocket)
-            self.web_sock_manager.send_wh00t_message(client_id, f'I left the chat, Bye.')
+            # self.web_sock_manager.send_wh00t_message(client_id, f'I left the chat, Bye.')
